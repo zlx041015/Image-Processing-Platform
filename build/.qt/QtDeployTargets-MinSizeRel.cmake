5d92b8979f860ed05dbf449dc828c9699179387a
zlx041015 <zlx041015@gmail.com>
2026-04-24 11:11:51 +0800

    添加gitignore忽略编译文件

--- build/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_BMPViewerQt_FILE E:/bmp_viewer_cpp/build/MinSizeRel/BMPViewerQt.exe)
-set(__QT_DEPLOY_TARGET_BMPViewerQt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_BMPViewerQt_RUNTIME_DLLS D:/QT/6.11.0/msvc2022_64/bin/Qt6Widgets.dll;D:/QT/6.11.0/msvc2022_64/bin/Qt6Gui.dll;D:/QT/6.11.0/msvc2022_64/bin/Qt6Core.dll)
